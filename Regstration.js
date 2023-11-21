3d4f162eba984fdaea6a4bb0c32cc1413aeee580
MANOJ SAI <123629429+Manojsai45@users.noreply.github.com>
2023-11-21 23:20:10 +0530

    Update Regstration.js

--- Regstration.js
+++ Regstration.js
@@ -1,114 +1,74 @@
-let addExampleEntry = () => {
-  cy.get("#name").type("Admin User 1");
-  cy.get("#email").type("admin1@example.com");
-  cy.get("#password").type("Test@123");
-  cy.get("#dob").click().type("2000-01-01");
-  cy.get("input[type=checkbox]").check();
-  cy.get("[type='submit']").click();
+/* Java script*/
+let userForm = document.getElementById("user-form");
+var UserEntries = [];
+
+let errors = [];
+const retieveEntries = () => {
+  let entries = localStorage.getItem("UserEntries");
+  if (entries) {
+    entries = JSON.parse(entries);
+  } else {
+    entries = [];
+  }
+  return entries;
+};
+const displayEntries = () => {
+  let entries = retieveEntries();
+  const tbleEntries = entries
+     .map((entry) => {
+      const nameCell = `<td class='border px-5 py-2'>${entry.name}</td>`;
+      const emailCell = `<td class='border px-5 py-2'>${entry.email}</td>`;
+      const passwordCell = `<td class='border px-5 py-2'>${entry.password}</td>`;
+      const dobCell = `<td class='border px-5 py-2'>${entry.dob}</td>`;
+      const acceptTermsCell = `<td class='border px-4 py-2'>${entry.acceptTerms}</td>`;
+      const row = `<tr>${nameCell} ${emailCell} ${passwordCell} ${dobCell} ${acceptTermsCell}</tr>`;
+      return row;
+    })
+    .join("\n");
+  const table = ` <table class='table-auto w-full'>
+    <tr>
+    <th class='px-4 py-2 '>Name </th>
+    <th class='px-4 py-2 '>Email </th>
+    <th class='px-4 py-2 '>Password </th>
+    <th class='px-4 py-2 '>Dob </th>
+    <th class='px-4 py-2 '>Accepted terms? </th>
+    </tr>${tbleEntries}
+</table>`;
+  let details = document.getElementById("user-entries");
+  details.innerHTML = table;
 };
 
-let validateExampleEntry = (data) => {
-  data.forEach((item) => {
-    cy.get("table").contains(item).should("exist");
-  });
+const saveUserForm = (event) => {
+  event.preventDefault();
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  const dob = document.getElementById("dob").value;
+  const acceptTerms = document.getElementById("acceptTerms").checked;
+  var currentYear = new Date().getFullYear();
+  var birthYear = dob.split("-");
+  let year = birthYear[0];
+  var age = currentYear - year;
+  console.log({ age, currentYear, birthYear });
+  if (age < 18 || age > 55) {
+    document.getElementById("dob").style = "border:1px solid red";
+    return alert("Your age must be under 18 and 55 years");
+  } else {
+    document.getElementById("dob").style = "border:none";
+
+    const entry = {
+      name,
+      email,
+      password,
+      dob,
+      acceptTerms,
+    };
+    UserEntries = retieveEntries();
+    UserEntries.push(entry);
+    localStorage.setItem("UserEntries", JSON.stringify(UserEntries));
+    displayEntries();
+    userForm.reset();
+  }
 };
-
-describe("", () => {
-  beforeEach(() => {
-    cy.visit(Cypress.env("STUDENT_SUBMISSION_URL"));
-  });
-
-  it("When loaded, the webpage should show these headings at the top of the table - Name, Email, Password, Dob, Accepted terms?", () => {
-    ["Name", "Email", "Password", "Dob", "Accepted terms?"].forEach((item) => {
-      cy.get("table").contains(item, { matchCase: false }).should("exist");
-    });
-  });
-
-  it("After the registration form is submitted, it should show an entry in the table immediately", () => {
-    addExampleEntry();
-    validateExampleEntry([
-      "Admin User 1",
-      "admin1@example.com",
-      "Test@123",
-      "2000-01-01",
-      "true",
-    ]);
-  });
-
-  it("After the registration form is submitted, it should show the entry added previously, even if the page is refreshed", () => {
-    addExampleEntry();
-    validateExampleEntry([
-      "Admin User 1",
-      "admin1@example.com",
-      "Test@123",
-      "2000-01-01",
-      "true",
-    ]);
-    cy.reload();
-    validateExampleEntry([
-      "Admin User 1",
-      "admin1@example.com",
-      "Test@123",
-      "2000-01-01",
-      "true",
-    ]);
-  });
-
-  it("It should be possible to use the registration form multiple times to see many entries in the table", () => {
-    addExampleEntry();
-    cy.reload();
-    cy.get("#name").type("Admin User 2");
-    cy.get("#email").type("admin2@example.com");
-    cy.get("#password").type("Test@321");
-    cy.get("#dob").click().type("1990-02-02");
-    cy.get("input[type=checkbox]").check();
-    cy.get("[type='submit']").click();
-
-    validateExampleEntry([
-      "Admin User 1",
-      "admin1@example.com",
-      "Test@123",
-      "2000-01-01",
-      "true",
-      "Admin User 2",
-      "admin2@example.com",
-      "Test@321",
-      "1990-02-02",
-      "true",
-    ]);
-  });
-
-  it("The registration form should not accept invalid email addresses", () => {
-    cy.get("#name").type("Admin User 4");
-    cy.get("#password").type("TestPass");
-    cy.get("#dob").click().type("1970-02-02");
-    cy.get("input[type=checkbox]").check();
-    // Should have correct email validation
-    cy.get("#email").type("admin2@example.com"); // Fix email address
-    cy.get("[type='submit']").click();
-    cy.get("table").find("tr").contains("admin2@example.com").should("not.exist");
-    // Should save when the error is resolved
-    cy.get("#email").type("@example.com");
-    cy.get("[type='submit']").click();
-    cy.get("table").find("tr").contains("admin2@example.com").should("exist");
-  });
-
-  it("The registration form should only accept users between 18 and 55 years old", () => {
-    cy.get("#name").type("Admin User 4");
-    cy.get("#password").type("TestPass");
-    cy.get("#email").type("admin@example.com");
-    cy.get("input[type=checkbox]").check();
-    // Should save when the error is resolved
-    cy.get("#dob").click().type("1998-02-02");
-    cy.get("[type='submit']").click();
-    cy.get("table").find("tr").contains("Admin User 4").should("exist");
-    // Should validate min age
-    cy.get("#dob").click().type("2010-02-02");
-    cy.get("[type='submit']").click();
-    cy.get("table").find("tr").contains("Admin User 4").should("not.exist");
-    // Should validate max age
-    cy.get("#dob").click().type("1960-02-02");
-    cy.get("[type='submit']").click();
-    cy.get("table").find("tr").contains("Admin User 4").should("not.exist");
-  });
-});
+userForm.addEventListener("submit", saveUserForm);
+displayEntries();
